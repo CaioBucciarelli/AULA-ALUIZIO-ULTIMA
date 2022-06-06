a0c13a05b329d173e3fdb6953ec702671d98461f
Caio Bucciarelli <82839819+CaioBucciarelli@users.noreply.github.com>
2022-06-06 10:50:29 -0300

    aula do dia 6/6

--- routes/atividades.js
+++ routes/atividades.js
@@ -1,3 +1,5 @@
+const atividades = require('../models/atividades')
+
 module.exports = (app)=>{
     app.post('/atividades',async(req,res)=>{
         //recuperando as informações digitadas
@@ -11,6 +13,7 @@ module.exports = (app)=>{
         //salvar as informações do formulario no database
         var salvar = await new atividades({
             data:dados.data,
+            disciplina:dados.disciplina,
             tipo:dados.tipo,
             entrega:dados.entrega,
             instrucoes:dados.orientacao,
@@ -21,5 +24,19 @@ module.exports = (app)=>{
         var buscar = await atividades.find({usuarios:dados.id})
         console.log(buscar)
         res.render('atividades.ejs',{nome:dados.nome,id:dados.id,dados:buscar})
+    })
+
+    //exluir atividades
+    app.get("/excluir",async(req,res)=>{
+        //recuperar o paramentro id da barra de endereço
+        var id = req.query.id
+        var excluir = await atividades.findOneAndRemove({
+            _id:id
+        })
+        //voltar para a página atividades
+        // res.render('atividades.ejs',{nome:dados.nome,id:dados.id,dados:buscar})
+        res.send("Atividades Excluída!")
+
+
     })
 }

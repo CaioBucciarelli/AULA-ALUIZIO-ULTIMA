7a398e7be1b319e246c32a1f91e043f037b262cb
Caio Bucciarelli <82839819+CaioBucciarelli@users.noreply.github.com>
2022-06-08 08:50:05 -0300

    aula do dia 8 do 6

--- routes/atividades.js
+++ routes/atividades.js
@@ -1,6 +1,20 @@
 const atividades = require('../models/atividades')
+const usuarios = require('../models/usuarios')
 
 module.exports = (app)=>{
+    //criar a rota para renderizar a view atividades
+    app.get('/atividades',async(req,res)=>{
+        //capturar o id da barra de endereço
+        var id = req.query.id
+        //buscar o nome na collection usuarios
+        var user = await usuarios.findOne({_id:id})
+        //buscar todas as atividades desse usuário
+        var buscar = await atividades.find({usuario:id})
+        //console.log(buscar)
+        res.render('atividades.ejs',{nome:user.nome,id:user._id,dados:buscar})
+    })
+
+    //gravar as informações do formulário na collection atividades
     app.post('/atividades',async(req,res)=>{
         //recuperando as informações digitadas
         var dados = req.body
@@ -17,13 +31,10 @@ module.exports = (app)=>{
             tipo:dados.tipo,
             entrega:dados.entrega,
             instrucoes:dados.orientacao,
-            usuarios:dados.id
+            usuario:dados.id
         }).save()
-
-        //buscar os documentos na coleção atividades desse usuário
-        var buscar = await atividades.find({usuarios:dados.id})
-        console.log(buscar)
-        res.render('atividades.ejs',{nome:dados.nome,id:dados.id,dados:buscar})
+        //redirecionar para a rota atividades
+        res.redirect('/atividades?id='+dados.id)
     })
 
     //exluir atividades
@@ -33,9 +44,8 @@ module.exports = (app)=>{
         var excluir = await atividades.findOneAndRemove({
             _id:id
         })
-        //voltar para a página atividades
-        // res.render('atividades.ejs',{nome:dados.nome,id:dados.id,dados:buscar})
-        res.send("Atividades Excluída!")
+        //redirecionar para a rota atividades
+        res.redirect('/atividades?id='+excluir.usuario)
 
 
     })
